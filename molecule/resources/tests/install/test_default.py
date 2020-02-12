6bfbd13c777b0aafb7a86beac70247c3bcd97f1e
Administrator <admin@example.com>
2020-02-12 23:07:17 +0000

    init

---
+++ molecule/resources/tests/install/test_default.py
@@ -0,0 +1,23 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_service(host):
+    services = ['httpd']
+    for s in services:
+        service = host.service(s)
+        assert service.is_running
+
+
+def test_installed_pkg(host):
+    assert host.package('httpd').is_installed
+
+
+def test_installed_config(host):
+    files = ['index.html']
+    for conf in files:
+        f = host.file('/var/www/html/' + conf)
+        assert f.exists
